79e954c4759cb8f3b0092e201a54afeb0a95500e
shubhamiscodding <shubham.modi.cg@gmail.com>
2025-02-20 16:35:34 +0530

    implement shadcn component

--- FrontEnd/finctrl/src/component/dashboard.jsx
+++ FrontEnd/finctrl/src/component/dashboard.jsx
@@ -1,9 +1,35 @@
-const Dashboard = () => {
-    return (
-        <>
-            <p></p>
-        </>
-    )
-}
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card"
 
-export default Dashboard;
+
+export default function Dashboard() {
+    return (
+      <div className="p-8">
+        <div className="grid md:grid-cols-1 lg:grid-cols-4">
+          <Card className="w-70">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Available Balance</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">$1,234.56</div>
+            </CardContent>
+          </Card>
+          <Card className="w-70">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total Expenses</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">$567.89</div>
+            </CardContent>
+          </Card>
+          <Card className="w-70">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total Income</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">$2,345.67</div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    )
+  }
