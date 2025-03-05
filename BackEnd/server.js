920c4a670064b802ed4b53e75d3490c5567996e4
shubhamiscodding <shubham.modi.cg@gmail.com>
2025-03-05 13:34:48 +0530

    edit the srver.js file

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -24,23 +24,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken'); // Import JWT
-const adminroutes = require('./routes/admin'); 
-const eventroutes = require('./routes/event');
-const userroutes = require('./routes/user');
+const adminRoutes = require('./routes/admin'); 
+const eventRoutes = require('./routes/event');
+const userRoutes = require('./routes/user');
+const financeRoutes = require("./routes/finance");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const JWT_SECRET = process.env.JWT_SECRET; // Load JWT secret key
+// const JWT_SECRET = process.env.JWT_SECRET; // Load JWT secret key
 
 app.use(express.json());
 app.use(cors());
 
 
-
-// Apply JWT middleware to protected routes
-app.use('/FinCtrl/admin', adminroutes);
-app.use('/FinCtrl/event', eventroutes);
-app.use('/FinCtrl/user', userroutes);
+app.use("/admin", adminRoutes);
+app.use("/users", userRoutes);
+app.use("/events", eventRoutes);
+app.use("/finance", financeRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
